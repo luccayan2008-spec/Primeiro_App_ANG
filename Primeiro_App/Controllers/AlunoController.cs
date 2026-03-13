5d5cf1396a13bd9fd4424b4f0ead4a35ee7c005b
luccayan2008-spec <luccayan2008@gmail.com>
2026-03-13 13:42:29 -0300

    Adicionando projeto

---
+++ Primeiro_App/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_App.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Primeiro_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Primeiro_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
